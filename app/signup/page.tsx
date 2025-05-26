67c5cbd5841699e358d3b4de008cb574c1ad48eb
jsam1904 <javierse.alvarado@gmail.com>
2025-05-26 06:59:50 -0600

    Actualizar opciones de género en el formulario de registro a "Masculino" y "Femenino"

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -168,9 +168,8 @@ export default function SignupPage() {
                   className="w-full border rounded px-3 py-2 bg-transparent focus:outline-none focus:ring-2 focus:ring-[#e6790c] dark:bg-[#04172d] dark:border-gray-600"
                 >
                   <option value="">Selecciona una opción</option>
-                  <option value="Hombre">Hombre</option>
-                  <option value="Mujer">Mujer</option>
-                  <option value="Otro">Otro</option>
+                  <option value="Masculino">Masculino</option>
+                  <option value="Femenino">Femenino</option>
                 </select>
               </div>
             </div>
